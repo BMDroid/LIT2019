d901b45713330e41fc3ef3d1ba11228cf54d462c
BMDroid <ametrojan@gmail.com>
2019-01-29 01:28:35 +0800

    create the data

--- createData.py
+++ createData.py
@@ -6,6 +6,13 @@ import numpy as np
 from collections import Counter
 from bs4 import BeautifulSoup
 
+def file_name_list(type):
+    fileList = os.listdir(f'./{type}')
+    if '.DS_store ' in fileList:
+        fileList.remove('.DS_store ')
+    fileNameList = list(map(lambda x: f'./{type}//' + x, fileList))
+    return fileNameList
+
 def read_html(fileName):
     html = open(fileName)
     soup = BeautifulSoup(html, 'html.parser') 
@@ -38,11 +45,11 @@ def found_in_text(lst, value):
 
 def clean_soup(soup, stopWords):
     case = list(soup.get_text().split())
-    puncs = string.punctuation.translate({ord('('): None, ord(')'): None}) + '’'
+    puncs = string.punctuation.translate({ord('('): None, ord(')'): None}) + '’' + '“' + '”'
     return list(map(lambda x: x.lower(), list(filter(lambda x:  4 < len(x) <= 20 and not any(p in x for p in puncs) and x not in stopWords and x.count('(') == x.count(')'), case))))
 
 def clean_text(lst, stopWords):
-    puncs = string.punctuation.translate({ord('('): None, ord(')'): None}) + '’'
+    puncs = string.punctuation.translate({ord('('): None, ord(')'): None}) + '’' + '“' + '”'
     return list(map(lambda x: x.lower(), list(filter(lambda x:  4 < len(x) <= 20 and not any(p in x for p in puncs) and x not in stopWords and x.count('(') == x.count(')'), lst))))
 
 def get_headings(classList):
@@ -59,44 +66,52 @@ def has_conclusion(soup, headingsList):
 def case_class(fileName):
     return fileName[-fileName[::-1].index('.') - 2]
 
-def tf_idf_vectorizer(cleanSoup, wordsBag):
+def tf_idf_vectorizer(cleanSoup, vecLength, wordsBag):
     length = len(cleanSoup)
-    vec = np.zeros(298) # win or lose then 299
+    vec = np.zeros(vecLength + 3)
     for idx, word in enumerate(wordsBag):
         vec[idx] = 1000 * cleanSoup.count(word) / length
     return vec
 
-def words_bag():
+def words_bag(type, dic):
     wordsBag = []
+    fileNameList = file_name_list(type)
     for fileName in fileNameList:
         soup = read_html(fileName)
-        wordsBag += clean_soup(soup, stopWords)
+        wordsBag.extend(clean_soup(soup, stopWords))
     wordsBagHist = dict(Counter(wordsBag))
-    sortedBag = sorted(wordsBagHist.items(), key=operator.itemgetter(1))
-    mostFreqWords = list(filter(lambda x: x[1] > 127, sortedBag))
+    sortedBag = sorted(wordsBagHist.items(), key=operator.itemgetter(1), reverse=True)
+    mostFreqWords = list(sortedBag[0: dic[type]])
     wordsBag = list(map(lambda x: x[0], mostFreqWords))
     return wordsBag
 
+def words_bag_combined(dic):
+    wordsBag = []
+    for type in dic:
+        wordsBag.extend(words_bag(type, dic))
+    wordsBag = list(set(wordsBag))
+    return len(wordsBag), wordsBag
+
 
 if __name__ == '__main__':
-    fileList = os.listdir('./Section 8(2)(b)')
-    if '.DS_store ' in fileList:
-        fileList.remove('.DS_store ')
-    fileNameList = list(map(lambda x: './Section 8(2)(b)/' + x, fileList))
 
     stopFileName = 'stopWords.txt'
     stopWords = read_stop_words(stopFileName)
+    dic = {'1':300, '2':50, '3': 650}
     
-    '''
-    wordsBag = words_bag()
-    with open('wordsBag.pkl', 'wb') as f:
-        pickle.dump(wordsBag, f)
-    '''
-    with open('wordsBag.pkl', 'rb') as f:
+    # vecLength, wordsBagCombined = words_bag_combined(dic)
+    # dic['combined']= vecLength # vecLength = 679
+    dic['combined'] = 679
+    wordsBagName = 'wordsBagCombined.pkl'
+    # with open(wordsBagName, 'wb') as f:
+    #    pickle.dump(wordsBagCombined, f)
+    with open(wordsBagName, 'rb') as f:
         wordsBag = pickle.load(f)
-
+    
     dataList = []
-    for fileName in fileNameList:
+    type = 'combined'
+    fileNameListCombined = file_name_list(type)
+    for fileName in fileNameListCombined:
         soup = read_html(fileName)
         classList = list_classes(soup)
         headingList = get_headings(classList)
@@ -104,12 +119,13 @@ if __name__ == '__main__':
         winOrLose = 1 if 'OS' in fileName else 0
         caseClass = case_class(fileName)
         cleanSoup = clean_soup(soup, stopWords)
-        vec = tf_idf_vectorizer(cleanSoup, wordsBag)
-        vec[295] = int(hasConclusion)
-        vec[296] = winOrLose
-        vec[297] = caseClass
+        vec = tf_idf_vectorizer(cleanSoup, dic[type], wordsBag)
+        vec[-3] = int(hasConclusion)
+        vec[-2] = winOrLose
+        vec[-1] = caseClass
         dataList.append(vec)
+    dataName = 'data' + str(type) + '.txt'
+    np.savetxt(dataName, tuple(dataList))
 
-    np.savetxt("data.txt", tuple(dataList))
 
 
